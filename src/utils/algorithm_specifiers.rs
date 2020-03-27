88843fc64d2a89c69c51285ff2cd0bdf97e1cf00
Jesper Brynolf <jesper.brynolf@gmail.com>
2020-03-27 08:09:22 -0400

    Updated copyright information.
    Signed-off-by: Jesper Brynolf <jesper.brynolf@gmail.com>

--- src/utils/algorithm_specifiers.rs
+++ src/utils/algorithm_specifiers.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, Arm Limited, All Rights Reserved
+// Copyright 2019 Contributors to the Parsec project.
 // SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may
