dbadf61ce8010110d35230d9949b70f5bede0884
Ionut Mihalcea <ionut.mihalcea@arm.com>
2020-08-07 14:22:55 +0100

    Create a generic buffer implementation
    This commit implements a generic implementation of buffer types (those
    starting with `TPM2B_` in the spec) and migrates the existing native
    buffers to the new implementation.
    
    Signed-off-by: Ionut Mihalcea <ionut.mihalcea@arm.com>

--- src/structures/buffers/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Contributors to the Parsec project.
-// SPDX-License-Identifier: Apache-2.0
-pub mod auth;
-pub mod data;
-pub mod digest;
-pub mod max;
-pub mod max_nv;
-pub mod nonce;
-pub mod publickeyrsa;
