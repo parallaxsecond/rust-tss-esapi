e0e0a7dd90ce9a2b6f439d52782ec43ece7b9dcc
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-01-27 11:06:36 +0000

    Implement TPM2_Shutdown
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- tss-esapi/src/context/tpm_commands/startup.rs
+++ tss-esapi/src/context/tpm_commands/startup.rs
@@ -1,5 +1,7 @@
 use crate::{
-    constants::types::startup::StartupType, tss2_esys::Esys_Startup, Context, Error, Result,
+    constants::types::startup::StartupType,
+    tss2_esys::{Esys_Shutdown, Esys_Startup},
+    Context, Error, Result,
 };
 use log::error;
 
@@ -16,5 +18,23 @@ impl Context {
         }
     }
 
-    // Missing function: shutdown
+    /// Send a TPM2_SHUTDOWN command to the TPM
+    pub fn shutdown(&mut self, shutdown_type: StartupType) -> Result<()> {
+        let ret = unsafe {
+            Esys_Shutdown(
+                self.mut_context(),
+                self.optional_session_1(),
+                self.optional_session_2(),
+                self.optional_session_3(),
+                shutdown_type.into(),
+            )
+        };
+        let ret = Error::from_tss_rc(ret);
+        if ret.is_success() {
+            Ok(())
+        } else {
+            error!("Error while shutting down TPM: {}", ret);
+            Err(ret)
+        }
+    }
 }
