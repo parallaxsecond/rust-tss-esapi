6298b4c163637e00e6cf303b269f881bd84e73e6
Patrick Uiterwijk <patrick@puiterwijk.org>
2021-01-27 11:19:13 +0000

    Implement TPM2_PolicyRestart
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- tss-esapi/tests/context_tests.rs
+++ tss-esapi/tests/context_tests.rs
@@ -978,6 +978,66 @@ mod test_policies {
     use super::*;
 
     #[test]
+    fn test_policy_restart() {
+        let mut context = create_ctx_without_session();
+
+        let trial_session = context
+            .start_auth_session(
+                None,
+                None,
+                None,
+                SessionType::Trial,
+                Cipher::aes_256_cfb(),
+                HashingAlgorithm::Sha256,
+            )
+            .expect("Start auth session failed")
+            .expect("Start auth session returned a NONE handle");
+        let (trial_session_attributes, trial_session_attributes_mask) =
+            SessionAttributesBuilder::new()
+                .with_decrypt(true)
+                .with_encrypt(true)
+                .build();
+        context
+            .tr_sess_set_attributes(
+                trial_session,
+                trial_session_attributes,
+                trial_session_attributes_mask,
+            )
+            .expect("tr_sess_set_attributes call failed");
+
+        let dig = context.policy_get_digest(trial_session).unwrap();
+        assert_eq!(
+            dig,
+            Digest::try_from(vec![
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ])
+            .unwrap()
+        );
+
+        let mut digest_list = DigestList::new();
+        digest_list
+            .add(get_pcr_policy_digest(&mut context, true, true).0)
+            .unwrap();
+        digest_list
+            .add(get_pcr_policy_digest(&mut context, false, true).0)
+            .unwrap();
+        context.policy_or(trial_session, digest_list).unwrap();
+
+        context.policy_restart(trial_session).unwrap();
+
+        let dig = context.policy_get_digest(trial_session).unwrap();
+        assert_eq!(
+            dig,
+            Digest::try_from(vec![
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ])
+            .unwrap()
+        );
+    }
+
+    #[test]
     fn test_policy_signed() {
         let mut context = create_ctx_with_session();
 
