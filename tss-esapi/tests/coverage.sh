7926f9044160f425f26b79a2b4c8dfe5dc9f9d9c
Ionut Mihalcea <ionut.mihalcea@arm.com>
2021-02-26 14:33:24 +0000

    Add code coverage reporting
    This commit adds a script and a nightly run for generating code coverage
    reports for the `tss-esapi` crate.
    
    Signed-off-by: Ionut Mihalcea <ionut.mihalcea@arm.com>

---
+++ tss-esapi/tests/coverage.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Contributors to the Parsec project.
+# SPDX-License-Identifier: Apache-2.0
+
+set -euf -o pipefail
+
+#################################
+# Run the TPM simulation server #
+#################################
+tpm_server &
+sleep 5
+tpm2_startup -c -T mssim
+
+#############################
+# Install and run tarpaulin #
+#############################
+cargo install cargo-tarpaulin
+cargo tarpaulin --tests --out Xml --exclude-files="tests/*,../*" -- --test-threads=1 --nocapture
+
+#################
+# Upload report #
+#################
+bash <(curl -s https://codecov.io/bash)
