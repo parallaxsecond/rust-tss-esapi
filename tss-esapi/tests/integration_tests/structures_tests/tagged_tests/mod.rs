738527256f94e0361bd906ebc1c037d9b1e78feb
Jesper Brynolf <jesper.brynolf@gmail.com>
2022-05-16 20:11:11 +0200

    Adds tests for SymmtericDefinitionObject.
    Signed-off-by: Jesper Brynolf <jesper.brynolf@gmail.com>

--- tss-esapi/tests/integration_tests/structures_tests/tagged_tests/mod.rs
+++ tss-esapi/tests/integration_tests/structures_tests/tagged_tests/mod.rs
@@ -6,5 +6,6 @@ mod public_rsa_exponent_tests;
 mod public_rsa_parameters_tests;
 mod sensitive;
 mod signature;
+mod symmetric_definition_object_tests;
 mod symmetric_definition_tests;
 mod tagged_signature_scheme_tests;
