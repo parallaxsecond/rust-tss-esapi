5620779472f2937fea9b67887bd31fc61005ec30
Ionut Mihalcea <ionut.mihalcea@arm.com>
2024-12-01 23:45:44 +0000

    Fix clippy warnings
    Signed-off-by: Ionut Mihalcea <ionut.mihalcea@arm.com>

--- tss-esapi/src/structures/nv/mod.rs
+++ tss-esapi/src/structures/nv/mod.rs
@@ -1,8 +1,8 @@
 // Copyright 2020 Contributors to the Parsec project.
 // SPDX-License-Identifier: Apache-2.0
 
-/// This module contains code that deals with the non volatile
-/// parts of the tpm.
+//! This module contains code that deals with the non volatile
+//! parts of the tpm.
 
 /// Non volatile storage module.
 pub mod storage;
